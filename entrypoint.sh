c2302473e9e9b31c99f67907397062c2a8aaa8a8
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-03-13 21:42:07 +0000

    Please check change.log

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,49 +4,50 @@
 Initialise(){
    lan_ip="$(hostname -i)"
    echo -e "\n"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    ***** Configuring SickGear container launch environment *****"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Local user: ${stack_user:=stackman}:${user_id:=1000}"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Local group: ${sickgear_group:=sickgear}:${sickgear_group_id:=1000}"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Password: ${stack_password:=Skibidibbydibyodadubdub}"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    SickGear application directory: ${app_base_dir:=/SickGear}"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    SickGear configuration directory: ${config_dir:=/config}"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Listening IP Address: ${lan_ip}"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    TV Show location(s): ${tv_dirs:=/storage/tvshows/}"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Download directory: ${tv_complete_dir:=/storage/downloads/complete/tv/}"
+   echo "$(date '+%c') INFO:    ***** Configuring SickGear container launch environment *****"
+   echo "$(date '+%c') INFO:    $(cat /etc/*-release | grep "PRETTY_NAME" | sed 's/PRETTY_NAME=//g' | sed 's/"//g')"
+   echo "$(date '+%c') INFO:    Local user: ${stack_user:=stackman}:${user_id:=1000}"
+   echo "$(date '+%c') INFO:    Local group: ${sickgear_group:=sickgear}:${sickgear_group_id:=1000}"
+   echo "$(date '+%c') INFO:    Password: ${stack_password:=Skibidibbydibyodadubdub}"
+   echo "$(date '+%c') INFO:    SickGear application directory: ${app_base_dir:=/SickGear}"
+   echo "$(date '+%c') INFO:    SickGear configuration directory: ${config_dir:=/config}"
+   echo "$(date '+%c') INFO:    Listening IP Address: ${lan_ip}"
+   echo "$(date '+%c') INFO:    TV Show location(s): ${tv_dirs:=/storage/tvshows/}"
+   echo "$(date '+%c') INFO:    Download directory: ${tv_complete_dir:=/storage/downloads/complete/tv/}"
 }
 
 CreateGroup(){
    if [ -z "$(getent group "${sickgear_group}" | cut -d: -f3)" ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Group ID available, creating group"
+      echo "$(date '+%c') INFO:    Group ID available, creating group"
       addgroup -g "${sickgear_group_id}" "${sickgear_group}"
    elif [ ! "$(getent group "${sickgear_group}" | cut -d: -f3)" = "${sickgear_group_id}" ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR:   Group SickGear group id mismatch - exiting"
+      echo "$(date '+%c') ERROR:   Group SickGear group id mismatch - exiting"
       exit 1
    fi
 }
 
 CreateUser(){
    if [ -z "$(getent passwd "${stack_user}" | cut -d: -f3)" ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    User ID available, creating user"
+      echo "$(date '+%c') INFO:    User ID available, creating user"
       adduser -s /bin/ash -H -D -G "${sickgear_group}" -u "${user_id}" "${stack_user}"
    elif [ ! "$(getent passwd "${stack_user}" | cut -d: -f3)" = "${user_id}" ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR:   User ID already in use - exiting"
+      echo "$(date '+%c') ERROR:   User ID already in use - exiting"
       exit 1
    fi
 }
 
 FirstRun(){
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    First run detected. Initialisation required"
+   echo "$(date '+%c') INFO:    First run detected. Initialisation required"
    find "${config_dir}" ! -user "${stack_user}" -exec chown "${stack_user}" {} \;
    find "${config_dir}" ! -group "${sickgear_group}" -exec chgrp "${sickgear_group}" {} \;
    su -p "${stack_user}" -c "/usr/bin/python ${app_base_dir}/sickgear.py --config ${config_dir}/sickgear.ini --datadir ${config_dir} --quiet --nolaunch --daemon --pidfile=/tmp/sickgear.pid"
    sleep 15
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Default configuration created - restarting"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    ***** Reload SickGear launch environment *****"
+   echo "$(date '+%c') INFO:    Default configuration created - restarting"
+   echo "$(date '+%c') INFO:    ***** Reload SickGear launch environment *****"
    pkill python
    sleep 5
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Configure update interval to 48hr"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Enable notifications for available updates"
+   echo "$(date '+%c') INFO:    Configure update interval to 48hr"
+   echo "$(date '+%c') INFO:    Enable notifications for available updates"
    sed -i \
       -e "/^\[General\]/,/^\[.*\]/ s%update_frequency =.*%update_frequency = 48%" \
       -e "/^\[General\]/,/^\[.*\]/ s%version_notify =.*%version_notify = 1%" \
@@ -63,16 +64,16 @@ FirstRun(){
 
 EnableSSL(){
    if [ ! -d "${config_dir}/https" ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Initialise HTTPS"
+      echo "$(date '+%c') INFO:    Initialise HTTPS"
       mkdir -p "${config_dir}/https"
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Generate server key"
+      echo "$(date '+%c') INFO:    Generate server key"
       openssl ecparam -genkey -name secp384r1 -out "${config_dir}/https/sickgear.key"
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Generate certificate request"
+      echo "$(date '+%c') INFO:    Generate certificate request"
       openssl req -new -subj "/C=NA/ST=Global/L=Global/O=SickGear/OU=SickGear/CN=SickGear/" -key "${config_dir}/https/sickgear.key" -out "${config_dir}/https/sickgear.csr"
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Generate certificate"
+      echo "$(date '+%c') INFO:    Generate certificate"
       openssl x509 -req -sha256 -days 3650 -in "${config_dir}/https/sickgear.csr" -signkey "${config_dir}/https/sickgear.key" -out "${config_dir}/https/sickgear.crt" >/dev/null 2>&1
    fi
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Configure SickGear to use HTTPS"
+   echo "$(date '+%c') INFO:    Configure SickGear to use HTTPS"
    if [ -f "${config_dir}/https/sickgear.key" ] && [ -f "${config_dir}/https/sickgear.crt" ]; then
       sed -i \
          -e "/^\[General\]/,/^\[.*\]/ s%https_key =.*%https_key = ${config_dir}/https/sickgear.key%" \
@@ -83,10 +84,10 @@ EnableSSL(){
 }
 
 Configure(){
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Disable browser launch on startup"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Configure host IP: ${lan_ip}"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Enable failed download handling"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Set TV Show library paths: ${tv_dirs}"
+   echo "$(date '+%c') INFO:    Disable browser launch on startup"
+   echo "$(date '+%c') INFO:    Configure host IP: ${lan_ip}"
+   echo "$(date '+%c') INFO:    Enable failed download handling"
+   echo "$(date '+%c') INFO:    Set TV Show library paths: ${tv_dirs}"
    sed -i \
       -e "/^\[General\]/,/^\[.*\]/ s%launch_browser =.*%launch_browser = 0%" \
       -e "/^\[General\]/,/^\[.*\]/ s%web_host =.*%web_host = ${lan_ip}%" \
@@ -106,17 +107,17 @@ Configure(){
          "${config_dir}/sickgear.ini"
    fi
    if [ "${global_api_key}" ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Enable API key usage"
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Set API key sabnzbd:::${global_api_key}"
+      echo "$(date '+%c') INFO:    Enable API key usage"
+      echo "$(date '+%c') INFO:    Set API key sabnzbd:::${global_api_key}"
       sed -i \
          -e "/^\[General\]/,/^\[.*\]/ s%use_api =.*%use_api = 1%" \
          -e "/^\[General\]/,/^\[.*\]/ s%api_keys =.*%api_keys = sabnzbd:::${global_api_key}%" \
          "${config_dir}/sickgear.ini"
    fi
    if [ "${sickgear_enabled}" ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Set web root for reverse proxying to /sickgear"
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Enable handling of reverse proxy headers"
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Disable handling of security headers; leave it to the NGINX reverse proxy"
+      echo "$(date '+%c') INFO:    Set web root for reverse proxying to /sickgear"
+      echo "$(date '+%c') INFO:    Enable handling of reverse proxy headers"
+      echo "$(date '+%c') INFO:    Disable handling of security headers; leave it to the NGINX reverse proxy"
       sed -i \
          -e "/^\[General\]/,/^\[.*\]/ s%web_root =.*%web_root = /sickgear%" \
          -e "/^\[General\]/,/^\[.*\]/ s%handle_reverse_proxy =.*%handle_reverse_proxy = 1%" \
@@ -128,7 +129,7 @@ Configure(){
 Kodi(){
    if [ "${kodi_enabled}" ]; then
       if [ "$(grep -c use_kodi "${config_dir}/sickgear.ini")" = 0 ]; then
-         echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Configuring kodi-headless"
+         echo "$(date '+%c') INFO:    Configuring kodi-headless"
          sed -i \
             -e "s%metadata_kodi =.*%metadata_kodi = 1|1|0|0|0|0|0|0|0|0%" \
             "${config_dir}/sickgear.ini"
@@ -142,7 +143,7 @@ Kodi(){
             -e "/^\[Kodi\]/a use_kodi = 1" \
             "${config_dir}/sickgear.ini"
       else
-         echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Configuring kodi-headless"
+         echo "$(date '+%c') INFO:    Configuring kodi-headless"
          sed -i \
             -e "/^\[Kodi\]/,/^\[.*\]/ s%kodi_host =.*%kodi_host = kodi:8080%" \
             -e "/^\[Kodi\]/,/^\[.*\]/ s%kodi_password =.*%kodi_password = ${kodi_password}%" \
@@ -159,9 +160,9 @@ Kodi(){
 
 SABnzbd(){
    if [ "${sabnzbd_enabled}" ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Enable SABnzbd"
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Setting SABnzbd host to https://sabnzbd:9090/"
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Setting SABnzbd category to: tv"
+      echo "$(date '+%c') INFO:    Enable SABnzbd"
+      echo "$(date '+%c') INFO:    Setting SABnzbd host to https://sabnzbd:9090/"
+      echo "$(date '+%c') INFO:    Setting SABnzbd category to: tv"
       sed -i \
          -e "/^\[General\]/,/^\[.*\]/ s%use_nzbs = 0%use_nzbs = 1%" \
          -e "/^\[General\]/,/^\[.*\]/ s%nzb_method =.*%nzb_method = sabnzbd%" \
@@ -195,7 +196,7 @@ SABnzbd(){
             -e "/^\[SABnzbd\]/,/^\[.*\]/ s%sab_host =.*%sab_host = https://sabnzbd:9090/%" \
             "${config_dir}/sickgear.ini"
       fi
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Setting SABnzbd API Key"
+      echo "$(date '+%c') INFO:    Setting SABnzbd API Key"
       if [ "$(grep -c sab_apikey "${config_dir}/sickgear.ini")" = 0 ]; then
          sed -i \
             -e "/^\[SABnzbd\]/a sab_apikey = ${global_api_key}" \
@@ -210,13 +211,13 @@ SABnzbd(){
 
 Deluge(){
    if [ "${deluge_enabled}" ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Enable Deluge for Torrents"
+      echo "$(date '+%c') INFO:    Enable Deluge for Torrents"
       sed -i \
          -e "/^\[General\]/,/^\[.*\]/ s%use_torrents =.*%use_torrents = 1%" \
          -e "/^\[General\]/,/^\[.*\]/ s%torrent_method =.*%torrent_method = deluge%" \
          "${config_dir}/sickgear.ini"
       if [ "$(grep -c "\[TORRENT\]" "${config_dir}/sickgear.ini")" = 0 ]; then
-         echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Add Deluge host configuration"
+         echo "$(date '+%c') INFO:    Add Deluge host configuration"
          echo "[TORRENT]" >> "${config_dir}/sickgear.ini"
          sed -i \
             -e "/^\[TORRENT\]/a torrent_password = ${stack_password}" \
@@ -225,7 +226,7 @@ Deluge(){
             -e "/^\[TORRENT\]/a torrent_label = tv" \
             "${config_dir}/sickgear.ini"
       else
-         echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Configure Deluge host"
+         echo "$(date '+%c') INFO:    Configure Deluge host"
          sed -i \
             -e "/^\[TORRENT\]/,/^\[.*\]/ s%torrent_password =.*%torrent_password = ${stack_password}%" \
             -e "/^\[TORRENT\]/,/^\[.*\]/ s%torrent_host =.*%torrent_host = https://deluge:8112/%" \
@@ -239,7 +240,7 @@ Deluge(){
 Prow(){
    if [ "${prowl_api_key}" ]; then
       if [ "$(grep -c "\[Prowl\]" "${config_dir}/sickgear.ini")" = 0 ]; then
-         echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Add Prowl notification configuration"
+         echo "$(date '+%c') INFO:    Add Prowl notification configuration"
          echo "[Prowl]" >> "${config_dir}/sickgear.ini"
          sed -i \
             -e "/^\[Prowl\]/a prowl_notify_ondownload = 1"\
@@ -247,7 +248,7 @@ Prow(){
             -e "/^\[Prowl\]/a use_prowl = 1" \
             "${config_dir}/sickgear.ini"
       fi
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Configure Prowl notifications"
+      echo "$(date '+%c') INFO:    Configure Prowl notifications"
       sed -i \
          -e "/^\[Prowl\]/,/^\[.*\]/ s%^use_prowl =.*%use_prowl = 1%" \
          -e "/^\[Prowl\]/,/^\[.*\]/ s%^prowl_api =.*%prowl_api = ${prowl_api_key}%" \
@@ -261,7 +262,7 @@ Prow(){
 
 OMGWTFNZBs(){
    if [ "${omgwtfnzbs_api_key}" ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Configuring OMGWTFNZBs search provider"
+      echo "$(date '+%c') INFO:    Configuring OMGWTFNZBs search provider"
       sed -i \
          -e "/^\[OMGWTFNZBS\]/,/^\[.*\]/ s%^omgwtfnzbs =.*%omgwtfnzbs = 1%" \
          -e "/^\[OMGWTFNZBS\]/,/^\[.*\]/ s%^omgwtfnzbs_username =.*%omgwtfnzbs_username = ${omgwtfnzbs_user}%" \
@@ -278,35 +279,35 @@ OMGWTFNZBs(){
 
 Indexers(){
    if [ "$(grep -c "\[MAGNETDL\]" "${config_dir}/sickgear.ini")" = 0 ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Enable MagnetDL provider"
+      echo "$(date '+%c') INFO:    Enable MagnetDL provider"
       echo "[MAGNETDL]" >> "${config_dir}/sickgear.ini"
       sed -i \
          -e "/^\[MAGNETDL\]/a magnetdl = 1" \
          "${config_dir}/sickgear.ini"
    fi
    if [ "$(grep -c "\[ETTV\]" "${config_dir}/sickgear.ini")" = 0 ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Enable ETTV provider"
+      echo "$(date '+%c') INFO:    Enable ETTV provider"
       echo "[ETTV]" >> "${config_dir}/sickgear.ini"
       sed -i \
          -e "/^\[ETTV\]/a ettv = 1" \
          "${config_dir}/sickgear.ini"
    fi
    if [ "$(grep -c "\[EZTV\]" "${config_dir}/sickgear.ini")" = 0 ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Enable EZTV provider"
+      echo "$(date '+%c') INFO:    Enable EZTV provider"
       echo "[EZTV]" >> "${config_dir}/sickgear.ini"
       sed -i \
          -e "/^\[EZTV\]/a eztv = 1" \
          "${config_dir}/sickgear.ini"
    fi
    if [ "$(grep -c "\[SKYTORRENTS\]" "${config_dir}/sickgear.ini")" = 0 ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Enable Sky Torrents provider"
+      echo "$(date '+%c') INFO:    Enable Sky Torrents provider"
       echo "[SKYTORRENTS]" >> "${config_dir}/sickgear.ini"
       sed -i \
          -e "/^\[SKYTORRENTS\]/a skytorrents = 1" \
          "${config_dir}/sickgear.ini"
    fi
    if [ "$(grep -c "\[SICK_BEARD_INDEX\]" "${config_dir}/sickgear.ini")" = 0 ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Enable SickBeard index provider"
+      echo "$(date '+%c') INFO:    Enable SickBeard index provider"
       echo "[SICK_BEARD_INDEX]" >> "${config_dir}/sickgear.ini"
       sed -i \
          -e "/^\[SICK_BEARD_INDEX\]/a sick_beard_index = 1" \
@@ -316,7 +317,7 @@ Indexers(){
 }
 
 SetOwnerAndGroup(){
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Correct owner and group of application files, if required"
+   echo "$(date '+%c') INFO:    Correct owner and group of application files, if required"
    find "${config_dir}" ! -user "${stack_user}" -exec chown "${stack_user}" {} \;
    find "${config_dir}" ! -group "${sickgear_group}" -exec chgrp "${sickgear_group}" {} \;
    find "${app_base_dir}" ! -user "${stack_user}" -exec chown "${stack_user}" {} \;
@@ -324,9 +325,9 @@ SetOwnerAndGroup(){
 }
 
 LaunchSickGear(){
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    ***** Configuration of SickGear container launch environment complete *****"
+   echo "$(date '+%c') INFO:    ***** Configuration of SickGear container launch environment complete *****"
    if [ -z "${1}" ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Starting SickGear as ${stack_user}"
+      echo "$(date '+%c') INFO:    Starting SickGear as ${stack_user}"
       exec "$(which su)" -p "${stack_user}" -c "$(which python) ${app_base_dir}/sickgear.py --config ${config_dir}/sickgear.ini --datadir ${config_dir}"
    else
       exec "$@"
