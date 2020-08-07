6707dc72c7f7eb3bc5416b45c2da46edb5e3e571
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-08-07 11:33:23 +0100

    Please check change.log

--- healthcheck.sh
+++ healthcheck.sh
@@ -5,5 +5,10 @@ if [ "$(netstat -plnt | grep -c 8081)" -eq 0 ]; then
    exit 1
 fi
 
+if [ "$(ip -o addr | grep "$(hostname -i)" | wc -l)" -eq 0 ]; then
+   echo "NIC missing"
+   exit 1
+fi
+
 echo "SickGear WebUI responding OK"
 exit 0
